34e47fa429b78ceb7a361d51cafac088a79d90b2
changmu <2276479303@qq.com>
2015-07-28 21:19:33 +0800

    update http_analysis.cpp

--- http_analysis.cpp
+++ http_analysis.cpp
@@ -29,6 +29,19 @@ map<string, string> http_analysis(int fd, size_t length)
         sscanf(buf + i, "%s%n", str, &skip_n);
         i += skip_n;
         mp[request_line[cnt]] = str;
+
+        if (1 == cnt) { // Url decompose
+            char *p = strchr(str, '?');
+            if (NULL == p) continue;
+            ++p;
+            char *pch = strtok(p, "&");
+            while (pch) {
+                p = strchr(pch, '=');
+                *p = '\0';
+                mp[pch] = p + 1;
+                pch = strtok(NULL, "&");
+            }
+        }
     }
     i += 2; // Skip "\r\n"
 
