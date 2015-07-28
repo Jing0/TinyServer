93f9040e0d58b632c1cc76a51ff977942998119d
vincent <adoyee@foxmail.com>
2015-07-28 20:09:26 +0800

    config interface

--- config.h
+++ config.h
@@ -16,10 +16,21 @@ struct config {
 	FILE *fp;
 	struct conflink *list;
 
+	// 不要用get_port这样的接口，不可能每次增加参数都增加类似的接口
 	int (*get_port)(config_t *);
+	// delete这个名字务必改掉
 	void (*delete)(config_t *);
 };
 
 config_t *config_new(const char *path);
+
+//建议的接口方式
+
+/*
+ * @ret 成功返回true, 失败或者section, key不存在返回false
+*/
+bool	config_get_int(config* c, const char* sec, const char* key, int* result);
+bool	config_get_string(config* c, const char* sec, const char* key, char** result);
+
 
 #endif
