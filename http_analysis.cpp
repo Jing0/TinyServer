2c72924c168c41725ab2e20bce463bb2405a614b
changmu <2276479303@qq.com>
2015-07-28 22:40:55 +0800

    update

--- http_analysis.cpp
+++ http_analysis.cpp
@@ -8,16 +8,32 @@
 
 using namespace std;
 
-void http_analysis(map<string, string> &mp, int fd, size_t length)
+void extra_fields(char *str, map<string, string> &mp)
+{
+    char *pch = strtok(p, "&");
+    while (pch) {
+        p = strchr(pch, '=');
+        *p = '\0';
+        mp[pch] = p + 1;
+        pch = strtok(NULL, "&");
+    }
+}
+
+/*
+ * buf stores HTTP request message, length is the message's length
+ * query is the field wanted to look up
+ * rst is a buffer to store the look up result
+ * */
+extern "C" void http_analysis(const char *buf, size_t length, const char *query, char *rst);
+void http_analysis(const char *buf, size_t length, const char *query, char *rst)
 {
     size_t i = 0, j = 0, skip_n;
     char *str = (char *) malloc(length + 1);
-    char *buf = (char *) malloc(length + 1);
-    recv(fd, buf, length, 0);
+    static map<string, string> mp; 
     string key;
     static const char *request_line[] = {
         "Method",
-        "URL",
+        "Url",
         "Version"
     };
 
@@ -27,19 +43,18 @@ void http_analysis(map<string, string> &mp, int fd, size_t length)
     for (int cnt = 0; cnt < 3; ++cnt) {
         sscanf(buf + i, "%s%n", str, &skip_n);
         i += skip_n;
+        if (1 == cnt) {
+            char *p = strchr(str, '?');
+            *p = '\0';
+            mp["Query"] = ++p;
+        }
         mp[request_line[cnt]] = str;
 
         if (1 == cnt) { // Url decompose
             char *p = strchr(str, '?');
             if (NULL == p) continue;
             ++p;
-            char *pch = strtok(p, "&");
-            while (pch) {
-                p = strchr(pch, '=');
-                *p = '\0';
-                mp[pch] = p + 1;
-                pch = strtok(NULL, "&");
-            }
+            extra_fields(str, mp);
         }
     }
     i += 2; // Skip "\r\n"
@@ -64,8 +79,16 @@ void http_analysis(map<string, string> &mp, int fd, size_t length)
         mp[key] = str;
     }
 
+    if (!strcmp("POST", mp["Method"].c_str())) {
+        strcpy(str, buf + i + 2);
+        mp["post_info"] = str;
+    }
+
     free(str);
-    free(buf);
+
+    /************************************/
+
+    strcpy(rst, mp[query].c_str());
 
     return;
 }
