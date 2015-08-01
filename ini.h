d94281ead60e672e3843e00e2a5f9c7110b87702
Jackie Kuo <j.kuo2012@gmail.com>
2015-08-01 16:35:27 +0800

    add the latest ini.h

--- ini.h
+++ ini.h
@@ -1,5 +1,16 @@
-#ifndef _CONFIG_H_
-#define _CONFIG_H_
+/*
+ * A simple ini file reader implementation in c
+ *
+ * The definition of the reader and it's methods
+ *
+ * File:   ini.h
+ * Author: Jackie Kuo <j.kuo2012@gmail.com>
+ *
+ * @license GNU AGPLv3
+ */
+
+#ifndef _INI_H_
+#define _INI_H_
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -22,13 +33,13 @@ typedef struct ini_section {
     struct ini_section *next;   /* next section */
 } ini_section;
 
-typedef struct config_t {
+typedef struct ini_t {
     ini_section *section;
-} config_t;
+} ini_t;
 
-config_t *config_new(const char *);
-void config_free(config_t *);
-bool config_getString(config_t *config, const char *section, const char *name, char *value);
-bool config_getInt(config_t *config, const char *section, const char *name, int *value);
+ini_t *ini_new(const char *);
+void ini_destroy(ini_t *);
+bool ini_getString(ini_t *config, const char *section, const char *name, char *value);
+bool ini_getInt(ini_t *config, const char *section, const char *name, int *value);
 
 #endif
